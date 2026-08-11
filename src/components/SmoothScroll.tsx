befa449885909843c71ffa90d9a4752a06b44589
Shamim Rana <shamimranaprofessional@gmail.com>
2026-08-11 19:07:20 +0600

    change logo and mobile device respnsive

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -6,14 +6,7 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
-      infinite: false,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
     });
 
     function raf(time: number) {
